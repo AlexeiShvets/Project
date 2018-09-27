1eb21002dc33236aeecc43c96286e7325cdc8f9b
AlexeiShvets <arekusei666@gmail.com>
2018-09-27 23:48:14 +0300

    два компонента фигура и клетка. частичная присвоение данных

---
+++ Kamisado/Assets/Scripts/Enums.cs
@@ -0,0 +1,23 @@
+﻿/// <summary>
+/// Разделение на белые и черные фигуры
+/// </summary>
+public enum UserType
+{
+    Black = 0,
+    White = 1
+}
+
+/// <summary>
+/// Цвета фигуры
+/// </summary>
+public enum ColorType
+{
+    Orange = 0,
+    Red = 1,
+    Green = 2,
+    Pink = 3,
+    Yellow = 4,
+    DarkBlue = 5,
+    Blue = 6,
+    Brown = 7
+}
---
+++ Kamisado/Assets/Scripts/Figure.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Figure : MonoBehaviour
+{
+    public ColorType Color;
+
+    public UserType UserType;
+
+    public int IdUser;
+
+    public Сell Cell;
+
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseDown() //функция клика мышью
+    {
+        if(Cell!=null)
+            Debug.Log(string.Format("{0} {1}", Cell.X, Cell.Y));
+        else
+            Debug.Log("null");
+
+        Cell = null;
+    }
+}
